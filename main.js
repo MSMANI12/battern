e7d9ac7f48f0644242f259c44ede49d1c8781d34
MSMANI12 <selvamani12121998@gmail.com>
2023-02-28 12:26:58 +0530

    add battern

---
+++ main.js
@@ -0,0 +1,37 @@
+var n=5
+function battern(n)
+{
+for (i=1;i<=n;i++)
+{
+for(j=1;j<=i;j++)
+{
+document.write(i);
+}
+document.write("<br>");
+}
+}
+battern(n)
+
+document.write("<br>");
+document.write("<br>");
+
+var n=5
+function reverse(n)
+{
+for (i=n;i>=1;i--)
+{
+for(j=1;j<=i;j++)
+{
+document.write(j);
+}
+document.write("<br>");
+}
+}
+reverse(n)
+
+
+
+
+
+
+
